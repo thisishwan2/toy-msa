178fdaa58154ba7b4d824b4b28fc375a3476af56
thisishwan2 <feel000617@gmail.com>
2024-06-01 17:36:06 +0900

    feat: filter 적용

--- first-service/src/main/java/com/example/firstservice/FirstController.java
+++ first-service/src/main/java/com/example/firstservice/FirstController.java
@@ -1,15 +1,24 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/first-service")
 public class FirstController {
 
     @GetMapping("/welcome")
     public String welcome(){
         return "welcome to the first service. ";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        log.info(header);
+        return "Hello World in First Service";
     }
 }

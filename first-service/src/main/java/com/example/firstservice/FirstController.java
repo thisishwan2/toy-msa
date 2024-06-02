2eb4b41900931b83ba62c34b34270dc14a5d1486
thisishwan2 <feel000617@gmail.com>
2024-06-02 16:52:10 +0900

    fix: 로드밸런싱 방식 변경

--- first-service/src/main/java/com/example/firstservice/FirstController.java
+++ first-service/src/main/java/com/example/firstservice/FirstController.java
@@ -1,6 +1,9 @@
 package com.example.firstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequestMapping("/first-service")
+@RequiredArgsConstructor
 public class FirstController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome(){
@@ -23,7 +29,8 @@ public class FirstController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. first service";
+    public String check(HttpServletRequest request){
+        log.info("Server port = {}", request.getServerPort());
+        return String.format("Hi, there. first service on Port %s", env.getProperty("local.server.port"));
     }
 }

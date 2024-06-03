e65d3e9dac8530e2aeb8fa88d37d8b18ce37570e
thisishwan2 <feel000617@gmail.com>
2024-06-03 12:40:48 +0900

    feat: health-check

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+
+    @Value("${greeting.message}")
+    private String value;
+
+    private final Greeting greeting;
+
+    @GetMapping("/health-check")
+    public String status(){
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+//        return value;
+        return greeting.getMessage();
+    }
+}

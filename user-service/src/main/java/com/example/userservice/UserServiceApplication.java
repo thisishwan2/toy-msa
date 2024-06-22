ae24b303e332b99f97b124bf9e45975a62bc9eeb
thisishwan2 <feel000617@gmail.com>
2024-06-22 14:43:11 +0900

    feat: feign client 사용

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient // Eureka 서버에 등록되는 클라이언트가 된다.
+@EnableFeignClients // FeignClient를 사용하기 위해 필요한 어노테이션
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
